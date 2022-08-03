fd5b3910cc0503b31356aafb0074f2be59ef794c
Ba Toi Nguyen <batoi1271.doe@gmail.com>
2022-08-03 22:39:12 +0700

    loader

--- js/main.js
+++ js/main.js
@@ -2,7 +2,19 @@ const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
 
+
+
+
+
+
+
 function start() {
+    // preloader 
+    const templatePreload = `<div id="preloader">
+	    <div class="lds-facebook"><div></div><div></div><div></div></div>
+    </div>`;
+    document.body.insertAdjacentHTML("beforebegin", templatePreload);
+
     if (bar) {
         bar.onclick = () => {
             nav.classList.add('active');
@@ -18,9 +30,17 @@ function start() {
     const product = document.querySelectorAll('.pro');
     product.forEach((pro) => {
         pro.onclick = () => {
-            window.location.href='s-product.html';
+            window.location.href = 's-product.html';
         }
     })
 }
 
 start();
+
+$(window).on('load', function () {
+    "use strict";
+    /*=========================================================================
+            Preloader
+    =========================================================================*/
+    $("#preloader").delay(750).fadeOut('slow');
+});
